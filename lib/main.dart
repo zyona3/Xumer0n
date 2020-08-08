7694ef5dbf688504baeb1ff587774815b3ae5c23
Xx0w0wxX <qx0w0xp@protonmail.com>
2020-08-08 14:02:36 +0900

    80% view

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,15 @@ final myFont = GoogleFonts.pressStart2p(
       const TextStyle(color: Colors.white, fontSize: 30, letterSpacing: 3),
 );
 
+final predictFont = GoogleFonts.pressStart2p(
+  textStyle:
+      const TextStyle(color: Colors.pinkAccent, fontSize: 30, letterSpacing: 3),
+);
+
 class _MyHomePageState extends State<MyHomePage> {
+  List<String> userList = ['432', '293', '093'];
+  List<String> cpuList = ['123', '892', '970'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -52,11 +60,65 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                   const SizedBox(height: 100),
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Text('You', style: myFont),
-                      Text('Input', style: myFont),
                       Text('CPU', style: myFont),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Expanded(
+                        child: ListView.builder(
+                          // padding: const EdgeInsets.all(50),
+                          shrinkWrap: true,
+                          itemBuilder: (BuildContext context, int index) {
+                            return Align(
+                                child:
+                                    Text(userList[index], style: predictFont));
+                          },
+                          itemCount: userList.length,
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      Expanded(
+                        child: Center(
+                          child: GridView.count(
+                            shrinkWrap: true,
+                            childAspectRatio:
+                                MediaQuery.of(context).size.width /
+                                    MediaQuery.of(context).size.height,
+                            crossAxisCount: 3,
+                            children: <Widget>[
+                              const Text('1'),
+                              const Text('2'),
+                              const Text('3'),
+                              const Text('4'),
+                              const Text('5'),
+                              const Text('6'),
+                              const Text('7'),
+                              const Text('8'),
+                              const Text('9'),
+                              const Text('Call'),
+                              const Text('0'),
+                              const Text('Back'),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: ListView.builder(
+                          // padding: const EdgeInsets.all(500),
+                          shrinkWrap: true,
+                          itemBuilder: (BuildContext context, int index) {
+                            return Align(
+                                child:
+                                    Text(userList[index], style: predictFont));
+                          },
+                          itemCount: cpuList.length,
+                        ),
+                      ),
                     ],
                   )
                 ],
